210d95eae8254b408bfb39dbeabf0c3d0cb41b4d
B0rnt0bew1ld <vstar8055@gmail.com>
2024-05-21 19:04:11 +0530

    hood design done

---
+++ www/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    
+    $('.text').textillate({
+        loop: true,
+        synk: true,
+        in:{
+            effect: "bounceIn",
+        },
+        out:{
+            effect: "bounceOut",
+        }
+    })
+});
