9be20a14521fe25d86783afa96e1cde64a94e562
B0rnt0bew1ld <136583094+B0rnt0bew1ld@users.noreply.github.com>
2024-07-01 14:55:48 +0530

    half done

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ eel.init("www")
 
 playAssistantSound()
 
-eel.start("index.html")
-
-os.system('start msedge.exe --app"http://localhost:8000/index.html"')
+os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
 eel.start("index.html", mode=None, host='localhost', block=True)
 
