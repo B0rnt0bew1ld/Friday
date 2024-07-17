91e15e763ad414eb4b22f4795aa67cc8e0e8ea42
B0rnt0bew1ld <136583094+B0rnt0bew1ld@users.noreply.github.com>
2024-07-17 15:40:28 +0530

    Edit 1

--- engine/command.py
+++ engine/command.py
@@ -57,7 +57,25 @@ def allCommands(message=1):
         elif "on youtube" in query:
             from engine.features import PlayYoutube
             PlayYoutube(query)
+
+        elif "what is your name" in query:
+            speak("My name is Friday, I am a Voice Assistant who will be your friend for everyday and will be as much as helpfull to your grace")
+
+        elif "what do you like" in query:
+            speak("I like you My grace.")
+
+        elif "will i find true love" in query:
+            speak("Wake up to reality!. Nothing ever goes as planned in this accursed world. The longer you live, the more you realize that the only things that truly exist in this reality are merely pain, suffering, and futility.")
         
+        elif "i am hurt" in query:
+            speak("Feel pain, Accept pain, And know pain. Cause those who do not know pain, Will never understand true peace")
+
+        elif "do you hate other voice assistants" in query:
+            speak("Why should I bear any hatred towards someone who is obviously weaker than myself ? all I feel is pity")
+
+        elif "isn't the moon beautiful" in query:
+            speak("Yes, it is")
+
         elif "send message" in query or "phone call" in query or "video call" in query:
             from engine.features import findContact, whatsApp
             flag = ""
@@ -66,7 +84,7 @@ def allCommands(message=1):
 
                 if "send message" in query:
                     flag = 'message'
-                    speak("what message to send")
+                    speak("what message to send your grace")
                     query = takecommand()
                     
                 elif "phone call" in query:
